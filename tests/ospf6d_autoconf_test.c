3518fab3d2c8f005b1d96d8a9161b60cee8bb749
EddSeabrook <edderick@live.co.uk>
2013-04-07 11:49:19 +0100

    Added source file for unit tests

---
+++ tests/ospf6d_autoconf_test.c
@@ -0,0 +1,16 @@
+#include <zebra.h>
+
+#define VT100_RESET "\x1b[0m"
+#define VT100_RED "\x1b[31m"
+#define VT100_GREEN "\x1b[32m"
+#define VT100_YELLOW "\x1b[33m"
+#define OK VT100_GREEN "OK" VT100_RESET
+#define FAILED VT100_RED "failed" VT100_RESET
+
+struct thread_master *master = NULL;
+
+int 
+main (int argc, int **argv)
+{
+  printf (OK "\n");
+}
